3a4822f323b2a796ff991d259e0333789fea014e
Justin <66831913+stuutzer@users.noreply.github.com>
2026-03-31 01:33:40 +1300

    FEAT: added EventService and UserService

---
+++ services/EventService.ts
@@ -0,0 +1,51 @@
+import prisma from "../lib/prisma";
+import { EventDTO, toEventDTO } from "@/dtos/event.dto";
+import { EventCreateInput, EventUpdateInput } from "@/app/generated/prisma/models";
+
+export class EventService {
+    static async getAllEvents(): Promise<EventDTO[]> {
+        const events = await prisma.event.findMany();
+        return events.map(toEventDTO);
+    }
+
+    static async getEventById(id: number): Promise<EventDTO | null> {
+        const event = await prisma.event.findUnique({ where: { id } });
+        if (!event) {
+            return null;
+        }
+        return toEventDTO(event);
+    }
+
+    static async createEvent(data: EventCreateInput): Promise<EventDTO> {
+        const event = await prisma.event.create({
+            data,
+        });
+
+        return toEventDTO(event);
+    }
+
+    static async updateEvent(
+        id: number,
+        data: EventUpdateInput
+    ): Promise<EventDTO> {
+        const event = await prisma.event.update({
+            where: { id },
+            data,
+        });
+
+        return toEventDTO(event);
+    }
+
+    static async deleteEvent(id: number) {
+        const event = await prisma.event.delete({
+            where: { id },
+        });
+
+        return event;
+    }
+
+    static async deleteAllEvents() {
+        const result = await prisma.event.deleteMany();
+        return result;
+    }
+}
