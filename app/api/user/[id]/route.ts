271cd46ecf89d92276bc4fb29ff992c5eb8c9f22
Justin <66831913+stuutzer@users.noreply.github.com>
2026-03-31 11:11:18 +1300

    FEAT: added API routes for user

---
+++ app/api/user/[id]/route.ts
@@ -0,0 +1,69 @@
+import { NextRequest, NextResponse } from "next/server";
+import { UserService } from "../../../../services/UserService";
+
+export async function GET(
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const { id } = await params;
+
+    try {
+        const user = await UserService.getUserById(id);
+
+        if (!user) {
+            return NextResponse.json(
+                { message: "User not found" },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json(user);
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json(
+            { message: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
+
+export async function DELETE(
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const { id } = await params;
+
+    try {
+        await UserService.deleteUser(id);
+        return NextResponse.json(
+            { message: "User deleted successfully" },
+            { status: 200 }
+        );
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json(
+            { message: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
+
+export async function PUT(
+    req: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const { id } = await params;
+    const data = await req.json();
+
+    try {
+        const updatedUser = await UserService.updateUser(id, data);
+        return NextResponse.json(
+            { message: "User updated successfully", data: updatedUser },
+            { status: 200 }
+        );
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json(
+            { message: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
