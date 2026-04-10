a129b8917de54c5b2a9ddd7d41d2ab82d585f9fb
vi0letchen <violethongjinchen@gmail.com>
2026-04-10 20:19:47 +1200

    refactor: remove null for env variables

--- app/actions/readFormData.ts
+++ app/actions/readFormData.ts
@@ -1,8 +1,8 @@
 "use server";
 import { createClient } from "@supabase/supabase-js";
 
-const apiKey = process.env.SUPABASE_SECRET_API_KEY ?? "";
-const supabase = createClient(process.env.SUPABASE_URL ?? "", apiKey);
+const apiKey = process.env.SUPABASE_SECRET_API_KEY!;
+const supabase = createClient(process.env.SUPABASE_URL!, apiKey);
 
 async function uploadThumbnail(thumbnail: File) {
     const ext = thumbnail.name.split(".").pop();
