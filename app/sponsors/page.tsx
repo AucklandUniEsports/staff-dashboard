4cbca4db4354fc91fd94341a6a42fa08f396f7d6
vi0letchen <violethongjinchen@gmail.com>
2026-05-15 21:25:20 +1200

    feat:implemented sponsors details page

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -1,19 +1,28 @@
 import Link from "next/link";
 import Table from "../components/Table";
 import prisma from '@/lib/prisma';
-import WorkInProgress from "../components/WorkInProgress";
+import SuccessToast from "../components/SuccessToast";
 
 const columns = ['name', 'tier'];
 export default async function Sponsors() {
   const rows = await prisma.sponsor.findMany();
   return (
       <>
-        {/* <Link className="action-block" href={"sponsors/add-sponsor"}>+ Add a Sponsor.</Link>
-        <section className="content-block">
-            <input className="input-field" type="text" placeholder="Search for an sponsor..."/>
-            <Table columns={columns} rows={rows}/>
-        </section> */}
-        <WorkInProgress/>
+          <SuccessToast />
+          <Link className="action-block" href={"sponsors/add-sponsor"}>
+              + Add a Sponsor.
+          </Link>
+          <Link className="action-block" href={"sponsors/add-sponsor-tier"}>
+              + Add Sponsor Tier.
+          </Link>
+          <section className="content-block">
+              <input
+                  className="input-field"
+                  type="text"
+                  placeholder="Search for a sponsor..."
+              />
+              <Table columns={columns} rows={rows} />
+          </section>
       </>
   );
 }
